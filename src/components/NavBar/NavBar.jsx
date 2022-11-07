2760216c84906f8956d28028e891c6def4f437b1
NahidulNoman <nahidulna5@gmail.com>
2022-11-07 23:01:13 +0600

    basic setup complete

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+// SWIPE FOR FOOD
+const NavBar = () => {
+
+  return (
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container">
+        <span class="navbar-brand">
+        SWIPE FOR FOOD
+        </span>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavDropdown">
+          <ul class="navbar-nav ">
+            <li class="nav-item">
+              <span class="nav-link active" aria-current="page">
+                Home
+              </span>
+            </li>
+            <li class="nav-item">
+              <span class="nav-link">
+                Features
+              </span>
+            </li>
+            <li class="nav-item">
+              <span class="nav-link" >
+                Pricing
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
