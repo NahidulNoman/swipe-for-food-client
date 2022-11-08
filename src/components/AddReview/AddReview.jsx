a8a27ae19e0a5fa84219a1ffbed6a27c15819fb5
NahidulNoman <nahidulna5@gmail.com>
2022-11-09 00:23:14 +0600

    basic add review implements

--- src/components/AddReview/AddReview.jsx
+++ src/components/AddReview/AddReview.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
 
 const AddReview = () => {
-    return (
-        <div>
-            <h3>please add review.</h3>
-        </div>
-    );
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const img = form.photoUrl.value;
+    const price = form.price.value;
+    const message = form.message.value;
+    const userReview = {
+      name,
+      img,
+      message,
+      price
+    };
+    console.log(userReview);
+
+    fetch('http://localhost:5000/services', {
+        method : 'post',
+        headers : {
+            'content-type' : 'application/json'
+        },
+        body : JSON.stringify(userReview)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+  };
+
+  return (
+    <div className="container">
+      <Form onSubmit={handlerSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label className="fw-semibold">Order Name</Form.Label>
+          <Form.Control
+            name="name"
+            type="text"
+            placeholder="Enter order name"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPhoto">
+          <Form.Label className="fw-semibold">Add Img</Form.Label>
+          <Form.Control
+            name="photoUrl"
+            type="text"
+            placeholder="add image"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPhoto">
+          <Form.Label className="fw-semibold">Order Price</Form.Label>
+          <Form.Control
+            name="price"
+            type="text"
+            placeholder="Enter price"
+          />
+        </Form.Group>
+        <InputGroup>
+        <InputGroup.Text className="fw-semibold">Description</InputGroup.Text>
+        <Form.Control name="message" type="text" as="textarea" aria-label="With textarea" placeholder="write your order Description..." />
+      </InputGroup>
+
+        <Button variant="primary" type="submit" className="fw-semibold mt-4 ">
+          POST
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default AddReview;
