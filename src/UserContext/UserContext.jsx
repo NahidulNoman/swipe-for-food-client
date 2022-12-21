a806e6b89b7a118979c6bfbf5cc652f549b011c3
NahidulNoman <nahidulna5@gmail.com>
2022-12-21 19:50:07 +0600

    context modify

--- src/UserContext/UserContext.jsx
+++ src/UserContext/UserContext.jsx
@@ -1,66 +1,71 @@
-import React, { createContext, useEffect, useState } from 'react';
-import app from '../firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 
 const auth = getAuth(app);
 export const AuthContext = createContext();
 
 const googleProvider = new GoogleAuthProvider();
 
-const UserContext = ({children}) => {
-    const [user , setUser] = useState({});
-    const [loading , setLoading] = useState(true);
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
 
-    const createUser = (email,password) => {
-        setLoading(true);
-        return createUserWithEmailAndPassword(auth,email,password);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signInGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const logUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    localStorage.removeItem("swipe-token");
+    setLoading(true);
+    return signOut(auth);
+  };
+  const userUpdateInfo = (profile) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, profile);
+  };
+
+  useEffect(() => {
+    const unSubscript = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+      setLoading(false);
+    });
+    return () => {
+      return unSubscript();
     };
-    const signInGoogle = () => {
-        setLoading(true);
-        return signInWithPopup(auth,googleProvider);
-    };
-    const logUser = (email,password) => {
-        setLoading(true);
-        return signInWithEmailAndPassword(auth,email,password);
-    };
-    const logOut = () => {
-        localStorage.removeItem('swipe-token');
-        setLoading(true);
-        return signOut(auth);
-    };
-    const userUpdateInfo = (profile) => {
-        setLoading(true);
-        return updateProfile(auth.currentUser,profile);
-    }
+  }, []);
 
-
-    useEffect( () => {
-        const unSubscript = onAuthStateChanged(auth, (loggedUser) => {
-            setUser(loggedUser);
-            setLoading(false);
-        });
-        return ()=> {
-            return unSubscript();
-        };
-    },[])
-
-
-    const authInfo = {
-        user,
-        loading,
-        setUser,
-        setLoading,
-        createUser,
-        logUser,
-        logOut,
-        userUpdateInfo,
-        signInGoogle,
-    }
-    return (
-        <AuthContext.Provider value={authInfo}>
-                {children}
-        </AuthContext.Provider>
-    );
+  const authInfo = {
+    user,
+    loading,
+    setUser,
+    setLoading,
+    createUser,
+    logUser,
+    logOut,
+    userUpdateInfo,
+    signInGoogle,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default UserContext;
